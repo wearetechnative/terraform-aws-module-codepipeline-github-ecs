daf32e6cdf6b457123e4c82cdbe1ed8fc18dcbb0
Wouter van der Toorren <githubwto@toorren.net>
2024-01-05 08:37:26 +0100

    make docker image for codepipeline variable

--- codebuildpipeline.tf
+++ codebuildpipeline.tf
@@ -20,7 +20,8 @@ resource "aws_codebuild_project" "default" {
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
     # image                       = "aws/codebuild/standard:7.0"
-    image = "221539347604.dkr.ecr.us-east-2.amazonaws.com/mustad/equinet_rails:latest"
+    # image = "221539347604.dkr.ecr.us-east-2.amazonaws.com/mustad/equinet_rails_dev:latest"
+    image = var.docker_run_image
     image_pull_credentials_type = "CODEBUILD"
     type                        = "LINUX_CONTAINER"
     privileged_mode             = "true" # for deployment to docker
@@ -92,6 +93,7 @@ resource "aws_codepipeline" "codepipeline" {
     # }
   }
 
+
   stage {
     name = "Source"
 
@@ -128,13 +130,14 @@ resource "aws_codepipeline" "codepipeline" {
       configuration = {
         ProjectName = "${var.pipeline_name}-Project" #"test"
       }
+
     }
   }
 
 
   dynamic "stage" {
 
-    for_each = var.build_stage_enabled ? [1] : []
+    for_each = var.deploy_to_ecs ? [1] : []
       content {
 
         name = "Deploy"
@@ -155,9 +158,6 @@ resource "aws_codepipeline" "codepipeline" {
         }
   }
 
-  provisioner "local-exec" {
-    command = "echo ${var.account} >> ${var.pipeline_name}.txt"
-  }
 }
 
 resource "aws_cloudwatch_log_group" "codepipeline_project" {
